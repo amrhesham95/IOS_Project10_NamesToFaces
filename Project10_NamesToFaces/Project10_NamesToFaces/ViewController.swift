69e27a01b04af152551094415683d977e82a0a52
Amr Hesham <amr.hesham@fawry.com>
2020-01-24 09:20:15 +0200

    creating the project

---
+++ Project10_NamesToFaces/Project10_NamesToFaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project10_NamesToFaces
+//
+//  Created by Amr Hesham on 1/24/20.
+//  Copyright © 2020 Amr Hesham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
