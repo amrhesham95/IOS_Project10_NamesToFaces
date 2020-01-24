eef79fe267c836bd3e4e207e5e6685c7d4152824
Amr Hesham <amr.hesham@fawry.com>
2020-01-24 11:09:22 +0200

    add saving the picture to the application document

--- Project10_NamesToFaces/Project10_NamesToFaces/ViewController.swift
+++ Project10_NamesToFaces/Project10_NamesToFaces/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else{
-            fatalError("unable to dequeue PersonCell")
+            fatalError("Unable to dequeue PersonCell")
         }
         return cell
     }
@@ -31,7 +31,27 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
         picker.delegate = self
         present(picker,animated: true)
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else{
+            return
+        }
+        let imageUniqueName = UUID().uuidString
+        let imagePath = getDocumentDirectory().appendingPathComponent(imageUniqueName)
+        
+        if let jpgData = image.jpegData(compressionQuality: 0.8){
+            try? jpgData.write(to: imagePath)
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func getDocumentDirectory() -> URL {
+        // used userDomainMask as said by Apple (if you open the class you will see that) that it should be used to save user's personal items
+        let Paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        // we got the value of the first element as the paths returned from the method contained only one element
+        return Paths[0]
+    }
 }
 
