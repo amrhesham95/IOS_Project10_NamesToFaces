700d8a4348dc873726af9e42e29eb078ebaecaf9
Amr Hesham <amr.hesham@fawry.com>
2020-01-24 10:44:43 +0200

    added the image picker button and picker now can be shown

---
+++ Project10_NamesToFaces/Project10_NamesToFaces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCellCollectionViewCell.swift
+//  Project10_NamesToFaces
+//
+//  Created by Amr Hesham on 1/24/20.
+//  Copyright © 2020 Amr Hesham. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var personNameLabel: UILabel!
+    @IBOutlet var personImageView: UIImageView!
+    
+}
--- Project10_NamesToFaces/Project10_NamesToFaces/ViewController.swift
+++ Project10_NamesToFaces/Project10_NamesToFaces/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
         // Do any additional setup after loading the view.
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else{
+            fatalError("unable to dequeue PersonCell")
+        }
+        return cell
+    }
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker,animated: true)
     }
 
 
