377017c570580bf2ee5286fa5fe3305c4ecc7e0d
Amr Hesham <amr.hesham@fawry.com>
2020-01-26 21:25:25 +0200

    adding ( alert controller with textfield , delete )

--- Project10_NamesToFaces/Project10_NamesToFaces/ViewController.swift
+++ Project10_NamesToFaces/Project10_NamesToFaces/ViewController.swift
@@ -25,8 +25,45 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
         }
         let person = people[indexPath.item]
         cell.personNameLabel.text = person.name
+        
+        //contentsOfFiles expects String so .path get the string value of the URL
         cell.personImageView.image = UIImage(contentsOfFile: getDocumentDirectory().appendingPathComponent(person.image).path)
+        
+        cell.personImageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.personImageView.layer.borderWidth = 2
+        cell.personImageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // to take text from popup alert controller
+        let ac = UIAlertController(title: "Rename", message: "Enter the person name", preferredStyle: .alert)
+        
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Ok", style: .default){
+            // to save the text coming from the textfield to the array and then refresh the collection view to see the effect
+            [weak self,weak ac] _ in
+            guard let newName = ac?.textFields?[0].text else{
+                return
+            }
+            
+            
+            self?.people[indexPath.row].name = newName
+            self?.collectionView.reloadData()
+        })
+        
+        ac.addAction(UIAlertAction(title: "Delete", style: .default){
+            // to save the text coming from the textfield to the array and then refresh the collection view to see the effect
+            [weak self,weak ac] _ in
+            self?.people.remove(at: indexPath.row)
+            self?.collectionView.reloadData()
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
     }
     @objc func addNewPerson() {
         let picker = UIImagePickerController()
@@ -42,9 +79,12 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
         let imageUniqueName = UUID().uuidString
         let imagePath = getDocumentDirectory().appendingPathComponent(imageUniqueName)
         
+        //converting the image to Data so we can write it on the disk
         if let jpgData = image.jpegData(compressionQuality: 0.8){
             try? jpgData.write(to: imagePath)
         }
+        
+        //creating person object after we have chosen a picture for him
         let person = Person(name: "person\(people.count)", image: imageUniqueName)
         people.append(person)
         collectionView.reloadData()
@@ -58,5 +98,7 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
         // we got the value of the first element as the paths returned from the method contained only one element
         return Paths[0]
     }
+    
+    
 }
 
